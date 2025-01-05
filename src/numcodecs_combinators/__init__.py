f7b9e5740c0886ad228a06ddf926c84069bcbeee
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-01-05 12:11:48 +0200

    Specify Python as code block language

--- src/numcodecs_combinators/__init__.py
+++ src/numcodecs_combinators/__init__.py
@@ -15,27 +15,27 @@ class CodecStack(Codec, tuple[Codec]):
     A stack of codecs, which makes up a combined codec.
 
     On encoding, the codecs are applied to encode from left to right, i.e.
-    ```
+    ```python
     CodecStack(a, b, c).encode(buf)
     ```
     computes
-    ```
+    ```python
     c.encode(b.encode(a.encode(buf)))
     ```
 
     On decoding, the codecs are applied to decode from right to left, i.e.
-    ```
+    ```python
     CodecStack(a, b, c).decode(buf)
     ```
     computes
-    ```
+    ```python
     a.decode(b.decode(c.decode(buf)))
     ```
 
     The [`CodecStack`][numcodecs_combinators.CodecStack] provides the additional
     [`encode_decode(buf)`][numcodecs_combinators.CodecStack.encode_decode]
     method that computes
-    ```
+    ```python
     stack.decode(stack.encode(buf))
     ```
     but makes use of knowing the shapes and dtypes of all intermediary encoding
